b0b993097f38b930873699d79b22bb6b8702a7b5
Petrov Aleksandr <petrovdeveloperdijicode@gmail.com>
2021-04-23 09:33:46 +0300

    added the gallery grid and finished the page layout discover

---
+++ src/components/gridListPhotos/gridListPhotos.tsx
@@ -0,0 +1,111 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import IconButton from '@material-ui/core/IconButton';
+import { title } from 'node:process';
+
+const tileData = [
+	{
+		img:
+			'https://cdn.pixabay.com/photo/2020/12/07/02/26/buildings-5810399__340.jpg',
+		title:
+			'https://cdn.pixabay.com/photo/2020/12/07/02/26/buildings-5810399__340.jpg',
+		author: 'author',
+		like: 1,
+	},
+	{
+		img:
+			'https://cdn.pixabay.com/photo/2020/12/07/02/26/buildings-5810399__340.jpg',
+		title:
+			'https://cdn.pixabay.com/photo/2020/12/07/02/26/buildings-5810399__340.jpg',
+		author: 'author',
+		like: 1,
+	},
+	{
+		img:
+			'https://cdn.pixabay.com/photo/2020/12/07/02/26/buildings-5810399__340.jpg',
+		title:
+			'https://cdn.pixabay.com/photo/2020/12/07/02/26/buildings-5810399__340.jpg',
+		author: 'author',
+		like: 1,
+	},
+	{
+		img:
+			'https://cdn.pixabay.com/photo/2020/12/07/02/26/buildings-5810399__340.jpg',
+		title:
+			'https://cdn.pixabay.com/photo/2020/12/07/02/26/buildings-5810399__340.jpg',
+		author: 'author',
+		like: 1,
+	},
+	{
+		img:
+			'https://cdn.pixabay.com/photo/2020/12/07/02/26/buildings-5810399__340.jpg',
+		title:
+			'https://cdn.pixabay.com/photo/2020/12/07/02/26/buildings-5810399__340.jpg',
+		author: 'author',
+		like: 1,
+	},
+	{
+		img:
+			'https://cdn.pixabay.com/photo/2020/12/07/02/26/buildings-5810399__340.jpg',
+		title:
+			'https://cdn.pixabay.com/photo/2020/12/07/02/26/buildings-5810399__340.jpg',
+		author: 'author',
+		like: 1,
+	},
+];
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		display: 'flex',
+		flexWrap: 'wrap',
+		justifyContent: 'space-around',
+		overflow: 'hidden',
+		backgroundColor: theme.palette.background.paper,
+	},
+	gridList: {
+		width: 500,
+		height: 450,
+	},
+	icon: {
+		color: 'rgba(255, 255, 255, 0.54)',
+	},
+}));
+
+export default function GridListPhotos() {
+	const classes = useStyles();
+
+	return (
+		<div className="wrapper-gallery">
+			<h2 className="title">Gallery</h2>
+			<div className={classes.root}>
+				<GridList cellHeight={180} className={classes.gridList}>
+					<GridListTile
+						key="Subheader"
+						cols={2}
+						style={{ height: 'auto' }}
+					></GridListTile>
+					{tileData.map((tile) => (
+						<GridListTile key={tile.img}>
+							<img src={tile.img} alt={tile.title} />
+							<GridListTileBar
+								title={tile.title}
+								subtitle={<span>by: {tile.author}</span>}
+								actionIcon={
+									<IconButton
+										aria-label={`info about ${tile.title}`}
+										className={classes.icon}
+									>
+										{tile.like}
+									</IconButton>
+								}
+							/>
+						</GridListTile>
+					))}
+				</GridList>
+			</div>
+		</div>
+	);
+}
--- src/pages/discover.tsx
+++ src/pages/discover.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import MainSlider from '../components/slider/slider';
+import GridListPhotos from '../components/gridListPhotos/gridListPhotos';
 
 const Discover = () => {
 	return (
 		<section className="discover">
 			<div className="container">
 				<MainSlider />
+				<GridListPhotos />
 			</div>
 		</section>
 	);
